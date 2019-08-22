90a08f57cc7ca4efe96c5fbc0b9ee75c4f579d58
Carlos Alhama Gil <carlos.alhamagil@plexus.es>
2019-08-22 09:32:12 +0200

    servicio creado

--- 02- angularBases/src/app/app.module.ts
+++ 02- angularBases/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './components/menu/menu.component';
 import { PagesModule } from './pages/pages.module';
+import { HttpClientModule  } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
