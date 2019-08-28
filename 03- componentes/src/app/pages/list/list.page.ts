89571087215427b2710350cd41e36eafe5dc1dd8
Carlos Alhama Gil <carlos.alhamagil@plexus.es>
2019-08-28 11:33:18 +0200

    ion-toast

--- 03- componentes/src/app/pages/list/list.page.ts
+++ 03- componentes/src/app/pages/list/list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -13,24 +13,32 @@ export class ListPage implements OnInit {
   usuarios: Observable<any>;
   @ViewChild('lista', {static: true}) lista: IonList;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private toastController: ToastController) { }
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
   }
 
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
   favorite(user) {
-    console.log('favorite', user);
+    this.presentToast('Se ha guardado en favoritos');
     this.lista.closeSlidingItems();
   }
 
   share(user) {
-    console.log('share', user);
+    this.presentToast('Compartido!');
     this.lista.closeSlidingItems();
   }
 
   borrar(user) {
-    console.log('borrar', user);
+    this.presentToast('Se ha borrado');
     this.lista.closeSlidingItems();
   }
 
