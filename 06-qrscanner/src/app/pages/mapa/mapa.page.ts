1350c7d4e6d02342e34133ffa28f0fe5f6368b9f
Carlos Alhama Gil <carlos.alhamagil@plexus.es>
2019-08-29 12:31:09 +0200

    se muestra mapa

--- 06-qrscanner/src/app/pages/mapa/mapa.page.ts
+++ 06-qrscanner/src/app/pages/mapa/mapa.page.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+declare var mapboxgl: any;
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage implements OnInit {
+export class MapaPage implements OnInit, AfterViewInit {
 
   latitud: number;
   longitud: number;
@@ -15,11 +17,20 @@ export class MapaPage implements OnInit {
 
   ngOnInit() {
     let geo: any = this.route.snapshot.paramMap.get('geo');
+
     geo = geo.substr(4);
     geo = geo.split(',');
 
     this.latitud = +geo[0];
     this.longitud = +geo[1];
+  }
+
+  ngAfterViewInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGF0ZXpubyIsImEiOiJjanp3ajJtbWUwMG1hM2JwNGNrb2dreWY5In0.nEKNqjeMeybCKz-tKnTm5A';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
   }
 
 }
