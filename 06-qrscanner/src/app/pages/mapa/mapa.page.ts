58f97f556dfa3e640416a5ee11d510a0c92a1517
Carlos Alhama Gil <carlos.alhamagil@plexus.es>
2019-08-29 12:42:07 +0200

    mapa 3d

--- 06-qrscanner/src/app/pages/mapa/mapa.page.ts
+++ 06-qrscanner/src/app/pages/mapa/mapa.page.ts
@@ -28,9 +28,55 @@ export class MapaPage implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     mapboxgl.accessToken = 'pk.eyJ1IjoicGF0ZXpubyIsImEiOiJjanp3ajJtbWUwMG1hM2JwNGNrb2dreWY5In0.nEKNqjeMeybCKz-tKnTm5A';
     const map = new mapboxgl.Map({
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [this.longitud, this.latitud],
+      zoom: 15.5,
+      pitch: 45,
+      bearing: -17.6,
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
-    });
+      antialias: true
+      });
+
+    map.on('load', () => {
+        map.resize();
+
+        new mapboxgl.Marker()
+          .setLngLat([this.longitud, this.latitud])
+          .addTo(map);
+
+        const layers = map.getStyle().layers;
+
+        let labelLayerId;
+        for (let i = 0; i < layers.length; i++) {
+          if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+          }
+        }
+
+        map.addLayer({
+          id: '3d-buildings',
+          source: 'composite',
+          'source-layer': 'building',
+          filter: ['==', 'extrude', 'true'],
+          type: 'fill-extrusion',
+          minzoom: 15,
+          paint: {
+          'fill-extrusion-color': '#aaa',
+          'fill-extrusion-height': [
+            'interpolate', ['linear'], ['zoom'],
+            15, 0,
+            15.05, ['get', 'height']
+          ],
+          'fill-extrusion-base': [
+            'interpolate', ['linear'], ['zoom'],
+            15, 0,
+            15.05, ['get', 'min_height']
+          ],
+          'fill-extrusion-opacity': .6
+          }
+        }, labelLayerId);
+      });
   }
 
 }
